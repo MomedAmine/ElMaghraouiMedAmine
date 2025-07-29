910f764474531b15e631acf5efff5d1371f043d8
Mohamed El Amine El Maghraoui <168415520+MomedAmine@users.noreply.github.com>
2025-07-29 17:12:48 +0000

    sync

--- src/elements/Exps/Exp/Exp.jsx
+++ src/elements/Exps/Exp/Exp.jsx
@@ -1,18 +1,18 @@
-import styles from "./Exp.module.css"
+import styles from "./Exp.module.css";
 function Exp(params) {
-    return (
-        <div className={styles.expWraper}>
-            <div className={styles.head}>
-                <h3>{params.role}</h3>
-                <p>{params.date}</p>
-            </div>
-            <div>
-                <p>{params.company}</p>
-            </div>
-            <div>
-                <p>{params.work}</p>
-            </div>
+  return (
+    <div className={styles.expWraper}>
+      <div className={styles.head}>
+        <h3 className={styles.divTtl}>{params.role}</h3>
+        <div>
+          <p className={styles.divTtl}>{params.company}</p>
+          <p style={{ transform: "translateY(-100%)" }}>{params.date}</p>
+          <div>
+            <p>{params.work}</p>
+          </div>
         </div>
-    )
+      </div>
+    </div>
+  );
 }
-export default Exp
+export default Exp;
