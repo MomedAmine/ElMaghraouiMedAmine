91e8b967d7318b1ba5026020ed1c215fa6206736
Momed PC <elmaghraoui.mohamedelamine@gmail.com>
2025-07-31 08:39:08 +0100

    RO add text

--- src/elements/RO/RO.jsx
+++ src/elements/RO/RO.jsx
@@ -14,9 +14,9 @@ function RO(params) {
         }
     return(
         <div className={styles.ros}>
-            <a target="_blank" rel="noopener noreferrer"  href="https://github.com/MomedAmine"><i class="fa-brands fa-github"></i></a>
-            <a target="_blank" rel="noopener noreferrer"  href="https://www.linkedin.com/in/mohamed-el-amine-el-maghraoui-1aa02a28b/"><i class="fa-brands fa-linkedin"></i></a>
-            <a target="_top" rel="noopener noreferrer"  href="mailto:elmaghraoui.mohamedelamine@gmail.com" onClick={onemailClick}><i class="fa-solid fa-envelope"></i></a>
+            <a target="_blank" rel="noopener noreferrer"  href="https://github.com/MomedAmine"><i class="fa-brands fa-github"></i><p> /MomedAmine</p></a>
+            <a target="_blank" rel="noopener noreferrer"  href="https://www.linkedin.com/in/mohamed-el-amine-el-maghraoui-1aa02a28b/"><i class="fa-brands fa-linkedin"></i><p> /in/mohamed-el-amine-el-maghraoui-1aa02a28b</p></a>
+            <a target="_top" rel="noopener noreferrer"  href="mailto:elmaghraoui.mohamedelamine@gmail.com" onClick={onemailClick}><i class="fa-solid fa-envelope"></i><p>elmaghraoui.mohamedelamine@gmail.com</p></a>
         </div>
     )
 }
--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,27 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base : process.env.VITE_BASE_PATH ||  "/ElMaghraouiMedAmine",
+  build: {
+    rollupOptions: {
+      output: {
+        chunkFileNames: 'assets/js/[name]-[hash].js',
+        entryFileNames: 'assets/js/[name]-[hash].js',
+        
+        assetFileNames: ({name}) => {
+          if (/\.(gif|jpe?g|png|svg)$/.test(name ?? '')){
+              return 'assets/images/[name]-[hash][extname]';
+          }
+          
+          if (/\.css$/.test(name ?? '')) {
+              return 'assets/css/[name]-[hash][extname]';   
+          }
+ 
+          // default value
+          // ref: https://rollupjs.org/guide/en/#outputassetfilenames
+          return 'assets/[name]-[hash][extname]';
+        },
+      },
+    }
+  },
 })
